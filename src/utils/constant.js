a263cd41b67f2c2d8814d473da8c954e36f586ff
hefeifan <281923396@qq.com>
2026-04-10 17:08:15 +0800

    修改基地址

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,12 +1,24 @@
 let urlParams = new URLSearchParams(window.location.search);
 let sid = urlParams.get('sid');
 let moduleId = urlParams.get('moduleId');
+let baseUrl = '';
+
+// 要匹配的前缀
+const prefix = 'https://cloud.feymer.com/bpm/';
+
+// 判断当前页面 URL 是否以此开头
+const isMatch = window.location.href.startsWith(prefix);
+
+if (isMatch) {
+  baseUrl = 'https://cloud.feymer.com/bpm/';
+} else {
+  baseUrl = 'https://cloud.feymer.com/bpmdevportal/';
+}
 
 const Config = {
     Sid: sid,
     ModuleId: moduleId,
-    BaseUrl: "https://cloud.feymer.com/bpmdevportal",
-    // BaseUrl: "https://cloud.feymer.com/bpm",
+    BaseUrl: baseUrl,
 }
 
 export default Config;
